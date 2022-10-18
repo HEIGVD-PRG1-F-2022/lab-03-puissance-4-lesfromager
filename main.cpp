d26a590d8267f09cc99ed1480d8983c75a0934a7
Cat-du-Fromage <46396184+Cat-du-Fromage@users.noreply.github.com>
2022-10-18 16:27:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ void playerInput(bool player, vector<vector<int>>& gameBoard);
 void displayGameBoard(const vector<vector<int>>& gameBoard);
 void victory();
 
+const int LIGNE = 6;
+const int COLONNE = 7;
 
 int main(){
-    vector<int> colGameBoard(7, 0);
-    vector<vector<int>> gameBoard(6, colGameBoard);
+    vector<int> xAxisGameBoard(7, 0);
+    vector<vector<int>> gameBoard(6, xAxisGameBoard);
 
-    displayRules();
+    //displayRules();
     displayGameBoard(gameBoard);
 
     bool playerTurn = 0;
@@ -31,7 +33,35 @@ int main(){
         playerTurn = winner ? playerTurn : !playerTurn;
     }
 
-    victory();
+    //victory();
     return 0;
 }
 
+void playerInput(bool player, vector<vector<int>>& gameBoard)
+{
+    int playerChar = !player ? 1 : 2;
+    int colonne = -1;
+
+    while (colonne > 0 && colonne < COLONNE)
+    {
+        cout << "Entrez le numéro de colonne :";
+        cin >> colonne;
+    }
+}
+
+void displayGameBoard(const vector<vector<int>>& gameBoard)
+{
+
+    for (int y = 0; y < gameBoard.size(); ++y) {
+        cout << "|";
+        for (int x = 0; x < gameBoard[y].size(); ++x) {
+            cout << " " << gameBoard.at(y).at(x) << " |";
+        }
+        cout << endl;
+    }
+
+    //Affichage de l'interface d'aide à l'utilisateur
+    for (int i = 1; i <= 7; ++i) {
+            cout << "  " << i << " ";
+    }
+}
