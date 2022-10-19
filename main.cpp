3acf2f1463f09041cf16f399655cd32fa4296b08
AJund <arthur.junod@heig-vd.ch>
2022-10-19 17:22:57 +0200

    PlayerInput et rajout fonction cellule libre

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ bool checkDiagonal(const vector<vector<int>>& gameBoard);
 void playerInput(bool player, vector<vector<int>>& gameBoard);
 void displayGameBoard(const vector<vector<int>>& gameBoard);
 void victory();
+int firstAvailableCell (const vector<vector<int>>& gameBoard, int colonne);
 
 const int LIGNE = 6;
 const int COLONNE = 7;
@@ -28,7 +29,7 @@ int main(){
         playerInput(playerTurn, gameBoard);
         displayGameBoard(gameBoard);
 
-        winner  = checkDiagonal(gameBoard) || checkHorizontal(gameBoard) || checkVertical(gameBoard);
+        //winner  = checkDiagonal(gameBoard) || checkHorizontal(gameBoard) || checkVertical(gameBoard);
 
         playerTurn = winner ? playerTurn : !playerTurn;
     }
@@ -41,12 +42,35 @@ void playerInput(bool player, vector<vector<int>>& gameBoard)
 {
     int playerChar = !player ? 1 : 2;
     int colonne = -1;
+    int firstCellAvailable = -1;
 
-    while (colonne > 0 && colonne < COLONNE)
+    int sizeBoard = gameBoard[0].size();
+
+    cout << sizeBoard;
+
+    do
     {
-        cout << "Entrez le numéro de colonne :";
+        cout << "Entrez le numero de colonne :";
         cin >> colonne;
+        if (colonne > 0 && colonne < sizeBoard){
+            cout << "Colonne non valide" << endl;
+            continue;
+        }
+
+        firstCellAvailable = firstAvailableCell(gameBoard, colonne);
+
+    }while ((colonne > 0 && colonne < sizeBoard) || firstCellAvailable != -1);
+
+    gameBoard.at(colonne).at(firstCellAvailable) = playerChar;
+}
+
+int firstAvailableCell (const vector<vector<int>>& gameBoard, int colonne){
+    for (int i = gameBoard.at(0).size(); i >= 0; i--){
+        if(gameBoard.at(colonne).at(i) == 0) {
+            return i;
+        }
     }
+    return -1;
 }
 
 void displayGameBoard(const vector<vector<int>>& gameBoard)
@@ -64,4 +88,5 @@ void displayGameBoard(const vector<vector<int>>& gameBoard)
     for (int i = 1; i <= 7; ++i) {
             cout << "  " << i << " ";
     }
+    cout << endl;
 }
